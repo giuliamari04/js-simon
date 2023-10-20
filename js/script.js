5d4de9d95c4b31cc322d4dfdb66687cf73c3424f
giuliamari04 <giuliamari04@gmail.com>
2023-10-20 15:52:31 +0200

    funzionamento base

---
+++ js/script.js
@@ -0,0 +1,69 @@
+memory();
+
+function memory() {
+    const numbers = document.getElementById('numbers');
+    const inputContainer = document.getElementById('input-container');
+    const numberInput = document.getElementById('number-input');
+    const btn = document.getElementById('button');
+    const result = document.getElementById('result');
+
+    const numeriDaGenerare = 5;
+    const max = 100;
+    const min = 1;
+    const numbersArray = [];
+
+
+    inputContainer.classList.add('d-none');
+    // Genera e visualizza numeri casuali
+    while (numbersArray.length < numeriDaGenerare) {
+        let randomNum = getRandomNumber(min, max);
+        if (!numbersArray.includes(randomNum)) {
+            numbersArray.push(randomNum);
+        }
+    }
+
+    numbers.innerHTML = `
+    <h2>ecco l'array di numeri:</h2> 
+    ${numbersArray.join(" ")}
+    </br>
+    <p>hai 10 secondi per memorizzarlo</p>
+    `;
+
+    setTimeout(function () {
+        numbers.textContent = "";
+        inputContainer.classList.remove('d-none');
+        numberInput.focus();
+    }, 2 * 1000);
+
+    btn.addEventListener("click", function () {
+        inputContainer.classList.add('d-none');
+
+        const inputText = numberInput.value;
+        const inputArray = inputText.split(" ");
+        const userNumbers = [];
+
+        for (let i = 0; i < inputArray.length; i++) {
+            const numberString = inputArray[i];
+            const number = parseInt(numberString);
+
+            if (!isNaN(number)) {
+                userNumbers.push(number);
+            }
+        }
+        //oppure si può scrivere anche con...
+        //const userNumbers = numberInput.value.split(" ").map(Number);
+
+        const correctNumbers = [];
+        for (let i = 0; i < numbersArray.length; i++) {
+            const num = numbersArray[i];
+            if (userNumbers.includes(num)) {
+                correctNumbers.push(num);
+            }
+        }
+        //oppure si puè anche scrivere così...
+        //const correctNumbers = numbersArray.filter(num => userNumbers.includes(num));
+
+        result.innerHTML = `<h1>Hai indovinato ${correctNumbers.length} numeri:</h1> <p>${correctNumbers.join(", ")}</p> </br> <p>l'array corretto è ${numbersArray}</p>`;
+    });
+};
+
